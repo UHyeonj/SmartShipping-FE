defe91eeec4726ed7e8bc762c6fc76593d0ac39a
UHyeonj <dngusdlqwkd@gmail.com>
2025-09-10 15:24:48 +0900

    feat: 기본 세팅

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,20 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
-import prettier from "eslint-config-prettier"; // prettier 추가
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import prettier from 'eslint-config-prettier'; // prettier 추가
 
 export default [
-  globalIgnores(["dist"]), // ① 검사 제외 디렉터리 설정
+  globalIgnores(['dist']), // ① 검사 제외 디렉터리 설정
   js.configs.recommended, // ② JS 기본 추천 규칙
   ...tseslint.configs.recommended, // ③ TypeScript ESLint 추천 규칙
-  reactHooks.configs["recommended-latest"], // ④ React Hooks 규칙
+  reactHooks.configs['recommended-latest'], // ④ React Hooks 규칙
   reactRefresh.configs.vite, // ⑤ React Fast Refresh 관련 규칙 (Vite 용)
   prettier, // ⑥ Prettier와 충돌 방지용 설정
   {
-    files: ["**/*.{ts,tsx}"], // ⑦ 타입스크립트 파일만을 위한 커스텀 설정
+    files: ['**/*.{ts,tsx}'], // ⑦ 타입스크립트 파일만을 위한 커스텀 설정
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
